08540d83781ed2439f0d0464b57b53319012872b
Yusuf Pathan <yusufp04@gmail.com>
2020-06-11 12:19:57 +0530

    changes

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 
 import React, { Fragment } from 'react';
-import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { Card, Form, Button, Alert, Badge } from 'react-bootstrap';
 import axios from 'axios';
 import { Redirect, Link } from 'react-router-dom';
 import '../App.css';
@@ -29,84 +29,92 @@ class Login extends React.Component {
   HandleSubmit = async (e) => {
     e.preventDefault();
     let message;
-    if (this.state.email == 'admin@admin.com' && this.state.password == 'admin'){
-      return (window.location.replace('/admin'))
+    if (
+      this.state.email == 'admin@admin.com' &&
+      this.state.password == 'admin'
+    ) {
+      return window.location.replace('/admin');
     }
-      axios
-        .get(
-          '/user/user?email=' +
-            this.state.email +
-            '&' +
-            'password=' +
-            this.state.password
-        )
-        .then((res) => {
-          this.setState({
-            message: res.data,
-          });
-          console.log(res.data);
-
-          window.localStorage.setItem('name', res.data.user.name);
-          window.localStorage.setItem('id', res.data.user._id);
-          window.localStorage.setItem('email', res.data.user.email);
-        })
-        .catch((err) => {
-          this.setState({
-            message: 'incorrect',
-          });
+    axios
+      .get(
+        '/user/user?email=' +
+          this.state.email +
+          '&' +
+          'password=' +
+          this.state.password
+      )
+      .then((res) => {
+        this.setState({
+          message: res.data,
         });
+        console.log(res.data);
+
+        window.localStorage.setItem('name', res.data.user.name);
+        window.localStorage.setItem('id', res.data.user._id);
+        window.localStorage.setItem('email', res.data.user.email);
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          message: 'incorrect',
+        });
+      });
   };
 
   render = () => {
     return (
-      <Fragment>
-        <Card>
-          <Card.Body>
-            <Card.Title>Login</Card.Title>
-            <Form onSubmit={this.HandleSubmit}>
-              <Form.Group controlId='formBasicEmail'>
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type='email'
-                  placeholder='Enter email'
-                  onChange={(event) => {
-                    this.HandleEmail(event.target.value);
-                  }}
-                />
-                <Form.Text className='text-muted'>
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+      <Fragment className='login'>
+        <div className='login'>
+          <h1 className='mb-3'>
+            <Badge variant='warning'>Login</Badge>
+          </h1>
+          <Card>
+            <Card.Body>
+              <Form onSubmit={this.HandleSubmit}>
+                <Form.Group controlId='formBasicEmail'>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type='email'
+                    placeholder='Enter email'
+                    onChange={(event) => {
+                      this.HandleEmail(event.target.value);
+                    }}
+                  />
+                  <Form.Text className='text-muted'>
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
 
-              <Form.Group controlId='formBasicPassword'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type='password'
-                  placeholder='Password'
-                  onChange={(event) => {
-                    this.HandlePassword(event.target.value);
-                  }}
-                />
-              </Form.Group>
-              <Button variant='primary' type='submit'>
-                Submit
-              </Button>
-            </Form>
-            <br />
-            {this.state.message == 'success' && (
-              <div>
-                <Alert variant='success'>Logged in sucessfully</Alert>
-                <Redirect to='/hotel' />
-              </div>
-            )}
-            {this.state.message == 'incorrect' && (
-              <Alert variant='danger'>Invalid Credentials</Alert>
-            )}
-            <Card.Text>
-              New User <Link to='/register'>Register</Link>
-            </Card.Text>
-          </Card.Body>
-        </Card>
+                <Form.Group controlId='formBasicPassword'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type='password'
+                    placeholder='Password'
+                    onChange={(event) => {
+                      this.HandlePassword(event.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Button variant='primary' type='submit'>
+                  Submit
+                </Button>
+              </Form>
+              <br />
+              {this.state.message == 'success' && (
+                <div>
+                  <Alert variant='success'>Logged in sucessfully</Alert>
+                  <Redirect to='/hotel' />
+                </div>
+              )}
+              {this.state.message == 'incorrect' && (
+                <Alert variant='danger'>Invalid Credentials</Alert>
+              )}
+              <Card.Text>
+                New User <Link to='/register'>Register</Link>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </Fragment>
     );
   };
--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 
 import React, { Fragment } from 'react';
-import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { Card, Form, Button, Alert, Badge } from 'react-bootstrap';
 import axios from 'axios';
 import { Redirect, Link } from 'react-router-dom';
 import '../App.css';
@@ -47,7 +47,7 @@ class Register extends React.Component {
       password: this.state.password,
       mobile: this.state.mobile,
     });
-    console.log(response.data)
+    console.log(response.data);
     this.setState({
       message: response.data.message,
     });
@@ -56,76 +56,80 @@ class Register extends React.Component {
   render = () => {
     return (
       <Fragment>
-        <Card>
-          <Card.Body>
-            <Card.Title>Login</Card.Title>
-            <Form onSubmit={this.HandleSubmit}>
-              <Form.Group>
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type='text'
-                  placeholder='Enter name'
-                  onChange={(event) => {
-                    this.HandleName(event.target.value);
-                  }}
-                />
-              </Form.Group>
-              <Form.Group controlId='formBasicEmail'>
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type='email'
-                  placeholder='Enter email'
-                  onChange={(event) => {
-                    this.HandleEmail(event.target.value);
-                  }}
-                />
-                <Form.Text className='text-muted'>
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+        <div className='login'>
+          <h1 className = "mb-3">
+            <Badge variant='warning'>Register</Badge>
+          </h1>
+          <Card>
+            <Card.Body>
+              <Form onSubmit={this.HandleSubmit}>
+                <Form.Group>
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type='text'
+                    placeholder='Enter name'
+                    onChange={(event) => {
+                      this.HandleName(event.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group controlId='formBasicEmail'>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type='email'
+                    placeholder='Enter email'
+                    onChange={(event) => {
+                      this.HandleEmail(event.target.value);
+                    }}
+                  />
+                  <Form.Text className='text-muted'>
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
 
-              <Form.Group controlId='formBasicPassword'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type='password'
-                  placeholder='Password'
-                  onChange={(event) => {
-                    this.HandlePassword(event.target.value);
-                  }}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Mobile Number</Form.Label>
-                <Form.Control
-                  type='number'
-                  placeholder='Mobile No.'
-                  onChange={(event) => {
-                    this.HandleMobile(event.target.value);
-                  }}
-                />
-              </Form.Group>
-              <Button variant='primary' type='submit'>
-                Submit
-              </Button>
-            </Form>
-            <br />
-            {this.state.message == 'created successfully' && (
-              <div>
-                <Alert variant='success'>{this.state.message}</Alert>
-                <Redirect to='/login' />
-              </div>
-            )}
-            {this.state.message == 'user exists' && (
-              <Alert variant='warning'>{this.state.message}</Alert>
-            )}
-            {this.state.message == 'user exists' && (
-              <Alert variant='warning'>{this.state.message}</Alert>
-            )}
-            <Card.Text>
-              Already Registered <Link to='/'>Login</Link>
-            </Card.Text>
-          </Card.Body>
-        </Card>
+                <Form.Group controlId='formBasicPassword'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type='password'
+                    placeholder='Password'
+                    onChange={(event) => {
+                      this.HandlePassword(event.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Mobile Number</Form.Label>
+                  <Form.Control
+                    type='number'
+                    placeholder='Mobile No.'
+                    onChange={(event) => {
+                      this.HandleMobile(event.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Button variant='primary' type='submit'>
+                  Submit
+                </Button>
+              </Form>
+              <br />
+              {this.state.message == 'created successfully' && (
+                <div>
+                  <Alert variant='success'>{this.state.message}</Alert>
+                  <Redirect to='/login' />
+                </div>
+              )}
+              {this.state.message == 'user exists' && (
+                <Alert variant='warning'>{this.state.message}</Alert>
+              )}
+              {this.state.message == 'user exists' && (
+                <Alert variant='warning'>{this.state.message}</Alert>
+              )}
+              <Card.Text>
+                Already Registered <Link to='/'>Login</Link>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </Fragment>
     );
   };
